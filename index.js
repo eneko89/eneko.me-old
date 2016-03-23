777ec98902e312457594e65e70b55b2ac1e530f6
Eneko <contact@eneko.me>
2016-03-23 18:11:06 +0100

    Implemented '/sendmail' endpoint with Nodemailer to send mails on behalf of <contact@eneko.me>

--- index.js
+++ index.js
@@ -7,29 +7,53 @@
  */
 
 var express = require('express'),
+    bodyParser = require('body-parser'),
     path = require('path'),
-    time = require('simple-time'),
-    request = require('request'),
-    OAuth = require('oauth-1.0a');
+    time = require('simple-time');
 
 // Express app and global middleware.
 var app = express(),
     morgan = require('morgan'),
     compression = require('compression');
 
-// Twitter OAuth API token, consumer key and secrets.
+// Request and oauth-1.0a for sending authenticated requests to the
+// Twitter API on behalf of '@enekodev' user.
+var request = require('request'),
+    OAuth = require('oauth-1.0a');
+
+// Nodemailer and xoauth2 for sending mails through Gmail on behalf
+// of <contact@eneko.me> (a Google Apps account with custom domain).
+var nodemailer = require('nodemailer'),
+    xoauth2 = require('xoauth2');
+
+// Twitter OAuth 1.0a API token, consumer key and their secrets.
 var twOAuth = OAuth({
-    consumer: {
-        public: process.env.TW_CONSUMER_KEY,
-        secret: process.env.TW_CONSUMER_SECRET
-    },
-    signature_method: 'HMAC-SHA1'
+  consumer: {
+      public: process.env.TW_CONSUMER_KEY,
+      secret: process.env.TW_CONSUMER_SECRET
+  },
+  signature_method: 'HMAC-SHA1'
 });
 
 var twToken = {
-    public: process.env.TW_TOKEN,
-    secret: process.env.TW_TOKEN_SECRET,
+  public: process.env.TW_TOKEN,
+  secret: process.env.TW_TOKEN_SECRET,
 };
+
+// Nodemailer's Gmail transporter with xoauth2 authentication. The
+// xoauth2 generator logs into the <contact@eneko.me> Gmail account
+// and generates the acces tokens for Nodemailer.
+var gmTransporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    xoauth2: xoauth2.createXOAuth2Generator({
+      user: 'contact@eneko.me',
+      clientId: process.env.GM_CLIENT_ID,
+      clientSecret: process.env.GM_CLIENT_SECRET,
+      refreshToken: process.env.GM_REFRESH_TOKEN
+    })
+  }
+});
 
 // Set server port.
 app.set('port', process.env.PORT || 3000);
@@ -43,6 +67,9 @@ app.use(compression());
 if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
+
+// Parse application/json content-type bodies (populates req.body).
+app.use(bodyParser.json());
 
 // Serve static files.
 // TODO: When things settle down a little bit, set Cache-Control header
@@ -90,6 +117,25 @@ app.get('/tweets', function(req, res) {
     }
   });
 
+});
+
+// Endpoint to send mails on behalf of the Gmail account defined in
+// nodemailer transporter's authentication service.
+app.get('/sendmail', function(req, res) {
+  gmTransporter.sendMail({
+      from: 'contact@eneko.me',
+      to: 'contact@eneko.me',
+      subject: '[eneko.me][From: '
+                + req.body.from + '] '
+                + req.body.subject,
+      text: req.body.text
+  }, function(error, resp) {
+     if (error) {
+        res.status(500).send({ error: 'Something blew up...' });
+     } else {
+        res.send();
+     }
+  });
 });
 
 // Reply with a 404 if nothing previously matches.
